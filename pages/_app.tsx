a5b03e5eeef9f7022a486aa25e8dd1da6011cbaf
James L. Odom <james@paycheck.io>
2026-03-03 20:03:24 -0800

    chore: add wagmi + query providers

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,17 @@
 import type { AppProps } from "next/app";
+import { useState } from "react";
+import { WagmiProvider } from "wagmi";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { wagmiConfig } from "../lib/wagmi";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [queryClient] = useState(() => new QueryClient());
+
+  return (
+    <WagmiProvider config={wagmiConfig}>
+      <QueryClientProvider client={queryClient}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
 }
